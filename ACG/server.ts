9c9d996897c473664ab8cb69594d14bd8d2a9acb
Christine Trites <ctrites1@my.bcit.ca>
2024-04-30 15:30:56 -0700

    updating files for styling... still need to get style.css with server.ts

--- ACG/server.ts
+++ ACG/server.ts
@@ -1,23 +1,26 @@
-import express, {Request, Response} from "express";
-import {createServer as createViteServer} from "vite";
+import express, { Request, Response } from "express";
+import { createServer as createViteServer } from "vite";
+import path from "node:path";
 
 const port = 5173;
 
 async function createServer() {
-  const app = express();
-  const vite = await createViteServer({
-    server: {middlewareMode: true},
-    appType: "custom",
-  });
-  app.use(vite.middlewares);
+	const app = express();
+	const vite = await createViteServer({
+		server: { middlewareMode: true },
+		appType: "custom",
+	});
 
-  app.get("/", async (req: Request, res: Response) => {
-    res.sendFile("src/areas/Arena/arena.html", {root: "."});
-  });
+	app.use(vite.middlewares);
+	app.use(express.static(path.join(__dirname, "public")));
 
-  app.listen(port, () => {
-    console.log(`server listening on port ${port}`);
-  });
+	app.get("/", async (req: Request, res: Response) => {
+		res.sendFile("src/areas/Arena/arena.html", { root: "." });
+	});
+
+	app.listen(port, () => {
+		console.log(`server listening on port ${port}`);
+	});
 }
 
 createServer();
