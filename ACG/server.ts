39aa75b348598febaddd59cc483b2758d8e178aa
keeles <kyleeeles@gmail.com>
2024-04-30 14:14:11 -0700

    setting up express server with vite

---
+++ ACG/server.ts
@@ -0,0 +1,22 @@
+import express, {Request, Response} from "express";
+import {createServer as createViteServer} from "vite";
+
+const port = 5173;
+
+async function createServer() {
+  const app = express();
+  const vite = await createViteServer({
+    server: {middlewareMode: true},
+    appType: "custom",
+  });
+  app.use(vite.middlewares);
+  app.use("*", async (req: Request, res: Response) => {
+    res.sendFile("dist/src/areas/Arena/arena.html", {root: "."});
+  });
+
+  app.listen(port, () => {
+    console.log(`server listening on port ${port}`);
+  });
+}
+
+createServer();
