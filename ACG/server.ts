ef426eb6d446459cee6acf61429329346433f957
keeles <kyleeeles@gmail.com>
2024-04-30 15:44:24 -0700

    setting up public directory

--- ACG/server.ts
+++ ACG/server.ts
@@ -1,26 +1,29 @@
-import express, { Request, Response } from "express";
-import { createServer as createViteServer } from "vite";
+import express, {Request, Response} from "express";
+import {createServer as createViteServer} from "vite";
+import {fileURLToPath} from "url";
 import path from "node:path";
 
 const port = 5173;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 async function createServer() {
-	const app = express();
-	const vite = await createViteServer({
-		server: { middlewareMode: true },
-		appType: "custom",
-	});
+  const app = express();
+  const vite = await createViteServer({
+    server: {middlewareMode: true},
+    appType: "custom",
+  });
 
-	app.use(vite.middlewares);
-	app.use(express.static(path.join(__dirname, "public")));
+  app.use(vite.middlewares);
+  app.use(express.static(path.join(__dirname, "public")));
 
-	app.get("/", async (req: Request, res: Response) => {
-		res.sendFile("src/areas/Arena/arena.html", { root: "." });
-	});
+  app.get("/", async (req: Request, res: Response) => {
+    res.sendFile("src/areas/Arena/arena.html", {root: "."});
+  });
 
-	app.listen(port, () => {
-		console.log(`server listening on port ${port}`);
-	});
+  app.listen(port, () => {
+    console.log(`server listening on port ${port}`);
+  });
 }
 
 createServer();
