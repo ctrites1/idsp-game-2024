b552a0ce7dc2a341d5868965d69a7abe4c3fc4c9
keeles <kyleeeles@gmail.com>
2024-05-02 12:03:33 -0700

    setting up vite proxy for api calls

--- ACG/server.ts
+++ ACG/server.ts
@@ -1,28 +1,13 @@
 import express, {Request, Response} from "express";
 import {createServer as createViteServer} from "vite";
 
-const port = 5173;
-
 async function createServer() {
   const app = express();
-  const vite = await createViteServer({
-    server: {middlewareMode: true},
-    appType: "custom",
-  });
-  app.use(vite.middlewares);
+  const port = 3000;
 
-  app.get("/", async (req: Request, res: Response) => {
-    // Change this to home page/login later
-    res.sendFile("src/areas/Arena/arena.html", {root: "."});
-  });
-
-  app.get("/play", async (req: Request, res: Response) => {
-    res.sendFile("src/areas/Arena/arena.html", {root: "."});
-  });
-
-  app.get("/instructions", async (req: Request, res: Response) => {
-    // Change this to how to page later
-    res.send("How to play");
+  // test route to make sure api calls working
+  app.get("/api/hello", async (req, res) => {
+    res.json({hello: "world"});
   });
 
   app.listen(port, () => {
