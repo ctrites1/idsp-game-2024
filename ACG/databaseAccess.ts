acfaffdbed06d3fa2de6ea35d7d9a76c6a3ebd45
Christine Trites <ctrites1@my.bcit.ca>
2024-05-07 11:14:46 -0700

    creating functions to create and remove players, generating demo players

--- ACG/databaseAccess.ts
+++ ACG/databaseAccess.ts
@@ -1,7 +1,70 @@
-import {database} from "./databaseConnection";
+import { database } from "./databaseConnection";
+import bcrypt from "bcrypt";
 
 async function getDeck() {
-  let getDeck = `
+	let getDeck = `
 
     `;
+}
+
+async function createPlayer(data: any) {
+	const saltRounds = 10;
+	bcrypt.genSalt(saltRounds, function (err, salt) {
+		bcrypt.hash(data.password, salt, function (err, hash) {
+			const createPlayerQuery = `
+        INSERT INTO player (username, email, password_hash)
+        VALUES ('${data.username}', '${data.email}', '${hash}')
+      `;
+
+			database
+				.query(createPlayerQuery)
+				.then(() => {
+					console.log("Player created successfully!");
+				})
+				.catch((error) => {
+					console.error("Error creating player:", error);
+
+					if (error.code === "ER_DUP_ENTRY") {
+						//TODO: display errors to client
+						if (error.sqlMessage.includes("username")) {
+							console.log("Username already in use.");
+						} else if (error.sqlMessage.includes("email")) {
+							console.log("Email already in use.");
+						}
+					} else {
+						console.log("An unexpected error occurred.");
+					}
+				});
+		});
+	});
+}
+
+const demoPlayer1 = {
+	username: "potat",
+	password: "strongPassword1",
+	email: "catrites@gmail.com",
+};
+
+const demoPlayer2 = {
+	username: "keeles",
+	password: "strongPassword2",
+	email: "kyleeeles@gmail.com",
+};
+
+/* -------------------------- Seeding Demo Players -------------------------- */
+// await createPlayer(demoPlayer1);
+// await createPlayer(demoPlayer2);
+
+async function removePlayerById(playerId: number) {
+	const removePlayerQuery = `
+    DELETE FROM player WHERE player_id = ${playerId};
+  `;
+	database
+		.query(removePlayerQuery)
+		.then(() => {
+			console.log("Player removed successfully...");
+		})
+		.catch((error) => {
+			console.error("Error deleting player from db: ", error);
+		});
 }
