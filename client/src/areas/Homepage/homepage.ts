c5fe300fca77ddcc3ae186781cef790addf2e96a
Christine Trites <ctrites1@my.bcit.ca>
2024-05-11 16:06:53 -0700

    i spent way too long animating 2 buttons

--- client/src/areas/Homepage/homepage.ts
+++ client/src/areas/Homepage/homepage.ts
@@ -1,35 +1,35 @@
 export async function createHomepage() {
-  const html: HTMLElement = document.documentElement;
-  const homepage: HTMLDivElement = document.createElement("div");
+	const html: HTMLElement = document.documentElement;
+	const homepage: HTMLDivElement = document.createElement("div");
 
-  homepage.className = "pseudo-homepage";
+	homepage.className = "pseudo-homepage";
 
-  const btn1: HTMLButtonElement = document.createElement("button");
-  // btn1.textContent = "Player 1";
-  btn1.className = "player1-btn";
+	const btn1: HTMLButtonElement = document.createElement("button");
+	// btn1.textContent = "Player 1";
+	btn1.className = "player-select player1-btn";
 
-  const btn2: HTMLButtonElement = document.createElement("button");
-  // btn2.textContent = "Player 2";
-  btn2.className = "player2-btn";
+	const btn2: HTMLButtonElement = document.createElement("button");
+	// btn2.textContent = "Player 2";
+	btn2.className = "player-select player2-btn";
 
-  const player1Img: HTMLImageElement = document.createElement("img");
-  const player2Img: HTMLImageElement = document.createElement("img");
+	const player1Img: HTMLImageElement = document.createElement("img");
+	const player2Img: HTMLImageElement = document.createElement("img");
 
-  const player1Label: HTMLHeadingElement = document.createElement("h1");
-  const player2Label: HTMLHeadingElement = document.createElement("h1");
-  player1Label.textContent = "Player 1";
-  player2Label.textContent = "Player 2";
+	const player1Label: HTMLHeadingElement = document.createElement("h1");
+	const player2Label: HTMLHeadingElement = document.createElement("h1");
+	player1Label.textContent = "Player 1";
+	player2Label.textContent = "Player 2";
 
-  btn1.appendChild(player1Img);
-  btn1.appendChild(player1Label);
-  btn2.appendChild(player2Img);
-  btn2.appendChild(player2Label);
+	btn1.appendChild(player1Img);
+	btn1.appendChild(player1Label);
+	btn2.appendChild(player2Img);
+	btn2.appendChild(player2Label);
 
-  const playerBtnDiv: HTMLDivElement = document.createElement("div");
-  playerBtnDiv.className = "player-selection-container";
+	const playerBtnDiv: HTMLDivElement = document.createElement("div");
+	playerBtnDiv.className = "player-selection-container";
 
-  playerBtnDiv.appendChild(btn1);
-  playerBtnDiv.appendChild(btn2);
-  homepage.appendChild(playerBtnDiv);
-  html.appendChild(homepage);
+	playerBtnDiv.appendChild(btn1);
+	playerBtnDiv.appendChild(btn2);
+	homepage.appendChild(playerBtnDiv);
+	html.appendChild(homepage);
 }
