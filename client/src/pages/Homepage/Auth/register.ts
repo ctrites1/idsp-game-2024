5883a2a3e6c5d6401e7501b56de8e215d8cb3971
Christine Trites <ctrites1@my.bcit.ca>
2024-05-22 16:08:29 -0700

    sorry Ademi I def thought I pooshed

--- client/src/pages/Homepage/Auth/register.ts
+++ client/src/pages/Homepage/Auth/register.ts
@@ -10,10 +10,13 @@ export async function createRegistrationPage() {
 	registerForm.method = "POST";
 	registerForm.action = "/api/register";
 
+	const title: HTMLHeadingElement = document.createElement("h1");
 	const username: HTMLInputElement = document.createElement("input");
 	const password: HTMLInputElement = document.createElement("input");
 	const email: HTMLInputElement = document.createElement("input");
 	const submit: HTMLButtonElement = document.createElement("button");
+
+	title.textContent = "Register New User";
 
 	username.setAttribute("placeholder", "username");
 	username.setAttribute("name", "username");
@@ -28,12 +31,13 @@ export async function createRegistrationPage() {
 	email.type = "email";
 
 	submit.type = "submit";
-	submit.innerText = "Login";
+	submit.innerText = "Register";
 
 	registerForm.appendChild(username);
 	registerForm.appendChild(password);
 	registerForm.appendChild(email);
 	registerForm.appendChild(submit);
+	formDiv.appendChild(title);
 	formDiv.appendChild(registerForm);
 	homepage.appendChild(formDiv);
 }
