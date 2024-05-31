91e4257718aa9004d92dd7a9a2c18136c5c79ae0
keeles <kyleeeles@gmail.com>
2024-05-30 23:48:19 -0700

    render deploy please

--- client/src/pages/Arena/roundCounter.ts
+++ client/src/pages/Arena/roundCounter.ts
@@ -1,7 +1,7 @@
-let currentRound = 1; // Initialize current round
+// let currentRound = 1; // Initialize current round
 
 export function updateRoundIndicator(round: number) {
-  currentRound = round; // Update current round
+  // currentRound = round; // Update current round
   const roundIndicator = document.querySelector(".round-indicator");
   if (roundIndicator) {
     roundIndicator.textContent = `Round ${round}/3`;
@@ -11,12 +11,14 @@ export function updateRoundIndicator(round: number) {
 export function markRoundWinner(round: number, winnerId: number) {
   const playerRoundLog = document.querySelector(".player-round-log");
   const oppRoundLog = document.querySelector(".opp-round-log");
-  const playerId = Number(document.querySelector("#playerHill")?.getAttribute("player-id"));
+  const playerId = Number(
+    document.querySelector("#playerHill")?.getAttribute("player-id")
+  );
 
   if (winnerId === playerId) {
     // Update the player round log
     const playerRound = playerRoundLog?.querySelector(`.player-round-${round}`);
-    console.log(`this is my round: ${round}`)
+    console.log(`this is my round: ${round}`);
     playerRound?.classList.add("win");
   } else {
     // Update the opponent round log
